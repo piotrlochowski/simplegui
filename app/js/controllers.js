4bf865b2fabd6f0dc9cc82690465ba2dd661e81b
Piotr Ziemniak <piotr.ziemniak.ext@impaqgroup.com>
2013-10-25 18:55:16 +0200

    which tab

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,15 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('GeneralForEvent', [function() {
+    controller('GeneralForEvent', [function () {
 
-  }])
-  .controller('GeneralForTrial', [function() {
+    }])
+    .controller('GeneralForTrial', [function () {
 
-  }]);
+    }])
+    .controller('Activation', function ($scope, $location) {
+        $scope.isActive = function (route) {
+            return route === $location.path();
+        }
+    });
+;
